2954ec84c2d8f2384e8e90e2d9844ee31c57dfe7
so_wap699 <t213616m@st.yamagata-u.ac.jp>
2021-12-04 22:21:33 +0900

    [AC] abc230 abc230_c

---
+++ atcoder.jp/abc230/abc230_c/Main.py
@@ -0,0 +1,14 @@
+n, a, b = map(int, input().split())
+p, q, r, s = map(int, input().split())
+
+H = q - p + 1
+W = s - r + 1
+
+for i in range(p, q+1):
+  ans = []
+  for j in range(r, s+1):
+    if (i - j == a - b) or (i + j == a + b):
+      ans.append('#')
+    else:
+      ans.append('.')
+  print(*ans, sep='')
