9ffd19e18e9634e0e69f2419176b8b63440520f1
so_wap699 <t213616m@st.yamagata-u.ac.jp>
2021-12-16 23:35:28 +0900

    [AC] abc217 abc217_b

---
+++ atcoder.jp/abc217/abc217_b/Main.py
@@ -0,0 +1,13 @@
+s = ['', '', '']
+s[0] = input()
+s[1] = input()
+s[2] = input()
+
+if 'ABC' not in s:
+  print('ABC')
+if 'ARC' not in s:
+  print('ARC')
+if 'AGC' not in s:
+  print('AGC')
+if 'AHC' not in s:
+  print('AHC')
