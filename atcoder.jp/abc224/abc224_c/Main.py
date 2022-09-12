2f83bd902e8212585ee90e326fe2e1a3f73d18fa
so_wap699 <t213616m@st.yamagata-u.ac.jp>
2022-09-12 23:58:02 +0900

    [AC] abc224 abc224_c

---
+++ atcoder.jp/abc224/abc224_c/Main.py
@@ -0,0 +1,11 @@
+n = int(input())
+p = [list(map(int,input().split())) for _ in range(n)]
+  
+ans = 0
+for i in range(n):
+  for j in range(i+1,n):
+    for k in range(j+1,n):
+      if (p[j][0]-p[i][0])*(p[k][1]-p[i][1])-(p[k][0]-p[i][0])*(p[j][1]-p[i][1])!=0:
+        ans += 1
+        
+print(ans)
